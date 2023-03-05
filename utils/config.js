e0b3eccf876d4bd750aa54439cbe4c8c22cf8899
unknown <henna.tuunainen@gmail.com>
2023-03-05 12:38:25 +0200

    Backendin testaaminen: test-ympäristö

--- utils/config.js
+++ utils/config.js
@@ -3,7 +3,10 @@
 require('dotenv').config()
 
 let PORT = process.env.PORT
-let MONGODB_URI = process.env.MONGODB_URI
+
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
 
 module.exports = {
   MONGODB_URI,
