5e79adc096ce4874fa69bdec0669a7f7863e0d2d
unknown <henna.tuunainen@gmail.com>
2023-03-05 15:30:05 +0200

    Backendin testaaminen: Try-catchin eliminointi

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const config = require('./utils/config')
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const notesRouter = require('./controllers/notes')
